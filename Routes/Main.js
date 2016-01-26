7862cf1536b94d9588b38a0963b1490f61e9eb4e
Miguel Aliaga <maliaga,pantoja@gmail.com>
2016-01-26 12:21:00 -0500

    new: add /version route

--- Routes/Main.js
+++ Routes/Main.js
@@ -1,9 +1,18 @@
 module.exports = function(server){
-  server.route({
-    method:'GET',
-    path:'/',
-    handler:function(request,reply){
-      reply('hello world!')
+  server.route([
+    {
+      method:'GET',
+      path:'/',
+      handler:function(request,reply){
+        reply('hello world!')
+      }
+    },
+    {
+      method:'GET',
+      path:'/version',
+      handler:function(request,reply){
+        reply('0.0.1')
+      }
     }
-  })
+])
 }
