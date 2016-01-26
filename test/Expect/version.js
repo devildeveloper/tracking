c2212c17b76eef64b03a3e360484e14a4edb8d47
Miguel Aliaga <maliaga,pantoja@gmail.com>
2016-01-26 12:21:31 -0500

    new: testing of new urls

---
+++ test/Expect/version.js
@@ -0,0 +1,5 @@
+module.exports={
+  statusCode : 200,
+  json       : false,
+  content    : '0.0.1'
+}
--- test/main.js
+++ test/main.js
@@ -2,6 +2,7 @@ const Expect   = require('chai').expect;
 const Request  = require('request');
 const Path     = 'http://localhost:3000/';
 const Root     = require('./Expect/root.js');
+const Version  = require('./Expect/version.js');
 
 describe('URL´s tesintg',function(){
   describe('root ("/") ',function(){
@@ -12,6 +13,18 @@ describe('URL´s tesintg',function(){
       },function(err,response,body){
         Expect(Root.statusCode).to.equal(response.statusCode);
         Expect(Root.content).to.equal(body)
+        done()
+      })
+    });
+  });
+  describe('version ("/version") ',function(){
+    it('should be return "0.0.1" ',function(done){
+      Request({
+        method:'GET',
+        uri   : Path+'version'
+      },function(err,response,body){
+        Expect(Version.statusCode).to.equal(response.statusCode);
+        Expect(Version.content).to.equal(body)
         done()
       })
     });
